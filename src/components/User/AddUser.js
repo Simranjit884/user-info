63d2a052227b1cd3f1ac36c7f848e4d13922985b
samy884 <ssgill884@gmail.com>
2022-05-03 22:25:03 +0530

    reseting input values

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -18,14 +18,17 @@ const AddUser = () => {
   const onAddUserHandler = (event) => {
     event.preventDefault();
     console.log(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
   };
+
   return (
     <Card className={classes.input}>
       <form onSubmit={onAddUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
         <label htmlFor="age">Age(In Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler}/>
+        <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}/>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
